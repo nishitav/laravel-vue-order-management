c634363fb5af9e66558de7984e9268e859e95536
nishitav <NishitVadhwana@mysale.com>
2024-04-16 16:09:04 +0530

    Readme file and other resource changes.

--- resources/js/Components/PrimaryButton.vue
+++ resources/js/Components/PrimaryButton.vue
@@ -1,36 +1,7 @@
 <template>
-  <span :class="badgeClasses">
-    <slot />
-  </span>
+    <button
+        class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150"
+    >
+        <slot />
+    </button>
 </template>
-
-<script>
-export default {
-  props: {
-    color: {
-      type: String,
-      default: 'gray',
-    },
-    rounded: {
-      type: Boolean,
-      default: false,
-    },
-    pill: {
-      type: Boolean,
-      default: false,
-    },
-  },
-  computed: {
-    badgeClasses() {
-      return [
-        'inline-block px-2 py-1 text-xs font-semibold leading-none',
-        `bg-${this.color}-200 text-${this.color}-800`,
-        {
-          'rounded-full': this.pill,
-          'rounded-md': this.rounded && !this.pill,
-        },
-      ];
-    },
-  },
-};
-</script>
